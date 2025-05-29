569509b997b64f2200a5f73919840da4dcbc8a8f
Erik Bolumburu <108585396+erikbolumburu11@users.noreply.github.com>
2025-05-29 21:37:30 +0100

    Can add and remove tasks!

--- src/routes/tasks.mjs
+++ src/routes/tasks.mjs
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createTask, getUserTasks, setTaskDone } from "../utils/taskQueries.mjs";
+import { createTask, deleteTask, getUserTasks, setTaskDone } from "../utils/taskQueries.mjs";
 
 const tasksRouter = Router();
 
@@ -7,8 +7,12 @@ tasksRouter.post('/new/', (request, response) => {
     return createTask(request.body.taskName, request.user.id, response);
 });
 
+tasksRouter.post('/delete/', (request, response) => {
+    return deleteTask(request.body.taskid, request.user.id, response);
+});
+
 tasksRouter.post('/setdone/', (request, response) => {
-    return setTaskDone(request.body.taskid, request.body.done, response);
+    return setTaskDone(request.body.taskid, request.body.done, request.user.id, response);
 });
 
 tasksRouter.get('/getusertasks/', (request, response) => {
--- src/utils/taskQueries.mjs
+++ src/utils/taskQueries.mjs
@@ -1,24 +1,38 @@
 import { db } from '../index.mjs'
 
 export function createTask(name, userid, response){
-        db.none('INSERT INTO tasks(name, done, userid) VALUES($1, $2, $3)',
+        db.one('INSERT INTO tasks(name, done, userid) VALUES($1, $2, $3) RETURNING id, name, done',
             [
                 name,
                 false,
                 userid
             ]
-        ).then(() => {
-            return response.status(200).send();
+        ).then((data) => {
+            return response.status(200).send({id: data.id, name: data.name, done: data.done});
         }).catch((error) =>{
             return response.status(400).send(error.toString());
         });
 }
 
-export function setTaskDone(taskid, done, response){
-    db.oneOrNone('UPDATE tasks SET done = $1 WHERE id = $2',
+export function setTaskDone(taskid, done, requestUserId, response){
+    db.oneOrNone('UPDATE tasks SET done = $1 WHERE id = $2 and userid = $3',
         [
             done,
-            taskid
+            taskid,
+            requestUserId
+        ]
+    ).then(() => {
+        return response.status(200).send();
+    }).catch((error) => {
+        return response.status(400).send(error.toString());
+    });
+}
+
+export function deleteTask(taskid, requestUserId, response){
+    db.oneOrNone('DELETE FROM tasks WHERE id = $1 AND userid = $2',
+        [
+            taskid,
+            requestUserId
         ]
     ).then(() => {
         return response.status(200).send();
