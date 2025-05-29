2733469832569f046a21bb784742b1772273838c
Erik Bolumburu <108585396+erikbolumburu11@users.noreply.github.com>
2025-05-29 17:24:33 +0100

    Can update tasks in tasks view

--- src/routes/tasks.mjs
+++ src/routes/tasks.mjs
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { createTask, getUserTasks } from "../utils/taskQueries.mjs";
+import { createTask, getUserTasks, setTaskDone } from "../utils/taskQueries.mjs";
 
 const tasksRouter = Router();
 
 tasksRouter.post('/new/', (request, response) => {
     return createTask(request.body.taskName, request.user.id, response);
+});
+
+tasksRouter.post('/setdone/', (request, response) => {
+    return setTaskDone(request.body.taskid, request.body.done, response);
 });
 
 tasksRouter.get('/getusertasks/', (request, response) => {
--- src/utils/taskQueries.mjs
+++ src/utils/taskQueries.mjs
@@ -14,6 +14,19 @@ export function createTask(name, userid, response){
         });
 }
 
+export function setTaskDone(taskid, done, response){
+    db.oneOrNone('UPDATE tasks SET done = $1 WHERE id = $2',
+        [
+            done,
+            taskid
+        ]
+    ).then(() => {
+        return response.status(200).send();
+    }).catch((error) => {
+        return response.status(400).send(error.toString());
+    });
+}
+
 export function getUserTasks(userid, response){
     db.manyOrNone('SELECT * FROM tasks WHERE userid = $1',
         [
